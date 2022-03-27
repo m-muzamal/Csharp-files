d36692006240e806bb2ac21749f3f29fa5a289ba
m-muzamal <91384189+m-muzamal@users.noreply.github.com>
2022-03-27 04:24:46 -0400

    Add files via upload

---
+++ check vowel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_statment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                char alphabet;
+                Console.Write("\nEnter an alphabet: ");
+                alphabet = Convert.ToChar(Console.ReadLine());
+
+                switch (Char.ToLower(alphabet))
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        Console.WriteLine("Vowel");
+                        break;
+                    default:
+                        Console.WriteLine("Not a vowel");
+                        break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
